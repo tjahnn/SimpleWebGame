7822a10ef23b16a2ca99dd0acd11ad3538773576
tjahnn <junbig8998@gmail.com>
2020-01-03 00:27:49 +0900

    add project
    add project

---
+++ scripts/loader.js
@@ -0,0 +1,25 @@
+﻿var jewel = {
+	screens : {}
+};
+
+//현재 문서가 완전히 로드될 때까지 기다린다.
+window.addEventListener("load", function(){
+	//동적 로딩으 시작한다.
+	Modernizr.load([
+		{
+			//항상 로드되는파일들
+			load: [
+				"scripts/sizzle.js",
+				"scripts/dom.js",
+				"scripts/game.js",
+				"scripts/screen.splash.js",
+				"scripts/screen.main-menu.js"
+			],
+			//모든 파일의 로드 및 실행이 완료되면 호출된다.
+			complete: function(){
+				jewel.game.showScreen("splash-screen");
+				console.log("모든 파일이 로드되었습니다!");
+			}			
+		}
+	]);
+}, false);
---
+++ shader/webgl-shader.js
@@ -0,0 +1,44 @@
+(function(root, factory) {  // eslint-disable-line
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], function() {
+      return factory.call(root);
+    });
+  } else {
+    // Browser globals
+    root.webglShader = factory.call(root);
+  }
+}(this, function() {
+  vertextShader = `
+    attribute vec4 a_position;
+    attribute vec4 a_color;
+    
+    uniform mat4 u_matrix;
+    
+    varying vec4 v_color;
+    
+    void main() {
+      // Multiply the position by the matrix.
+      gl_Position = u_matrix * a_position;
+    
+      // Pass the color to the fragment shader.
+      v_color = a_color;
+    }
+  `;
+
+  fragmentShader = `
+    precision mediump float;
+    
+    // Passed in from the vertex shader.
+    varying vec4 v_color;
+    
+    void main() {
+      gl_FragColor = v_color;
+    }
+  `;
+
+    return {
+      vertextShader,
+      fragmentShader,
+    };
+}));
