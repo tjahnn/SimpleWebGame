4dfa598958040822a7f38edff6f7c02aa618eca0
tjahnn <junbig8998@gmail.com>
2020-01-04 21:50:19 +0900

    add webgl main

---
+++ webgl_main.js
@@ -0,0 +1,174 @@
+(function(root, factory) {  // eslint-disable-line
+    if (typeof define === 'function' && define.amd) {
+      // AMD. Register as an anonymous module.
+      define([], function() {
+        return factory.call(root);
+      });
+    } else {
+      // Browser globals
+      root.webglMain = factory.call(root);
+    }
+  }(this, function() {
+    var cameraAngle = 0;
+    var cameraView = 0;
+    var program;
+    var positionLocation;
+    var colorLocation;
+    var matrixLocation;
+    var positionBuffer;
+    var colorBuffer;
+
+    function AddCameraAngle(dVal) {
+        cameraAngle += dVal;
+    }
+
+    function AddCameraView(dVal) {
+        cameraView += dVal;
+    }
+
+    function drawInit(gl) {
+        // setup GLSL program
+        program = webglUtils.createProgramFromScripts(gl, [[webglShader.vertextShader, gl.VERTEX_SHADER], [webglShader.fragmentShader, gl.FRAGMENT_SHADER]]);
+
+        // look up where the vertex data needs to go.
+        positionLocation = gl.getAttribLocation(program, "a_position");
+        colorLocation = gl.getAttribLocation(program, "a_color");
+
+        // lookup uniforms
+        matrixLocation = gl.getUniformLocation(program, "u_matrix");
+
+        // Create a buffer to put positions in
+        positionBuffer = gl.createBuffer();
+        // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        // Put geometry data into buffer
+        setGeometry(gl);
+
+        // Create a buffer to put colors in
+        colorBuffer = gl.createBuffer();
+        // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = colorBuffer)
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        // Put geometry data into buffer
+        setColors(gl);
+        function setGeometry(gl) {
+            var positions = webglfigure.getDefaultBody();
+            var matrix = webglm4.scaling(1, 1, 1);
+            
+            for (var ii = 0; ii < positions.length; ii += 3) {
+                var vector = webglm4.vectorMultiply([positions[ii + 0], positions[ii + 1], positions[ii + 2], 1], matrix);
+                positions[ii + 0] = vector[0];
+                positions[ii + 1] = vector[1];
+                positions[ii + 2] = vector[2];
+            }
+            
+            gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+        }
+            
+        // Fill the buffer with colors for the 'F'.
+        function setColors(gl) {
+            gl.bufferData(
+                gl.ARRAY_BUFFER,
+                webglfigure.getDefaultColor(),
+                gl.STATIC_DRAW);
+        }
+    }
+
+    function radToDeg(r) {
+        return r * 180 / Math.PI;
+    }
+    
+    function degToRad(d) {
+        return d * Math.PI / 180;
+    }
+
+    function drawScene() {
+        var cameraAngleRadiansY = degToRad(cameraAngle);
+        var cameraAngleRadiansX = degToRad(-45 + cameraView);
+        var fieldOfViewRadians = degToRad(60);
+        
+        webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+        
+        // Tell WebGL how to convert from clip space to pixels
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        
+        // Clear the canvas AND the depth buffer.
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        
+        // Turn on culling. By default backfacing triangles
+        // will be culled.
+        gl.enable(gl.CULL_FACE);
+        
+        // Enable the depth buffer
+        gl.enable(gl.DEPTH_TEST);
+        
+        // Tell it to use our program (pair of shaders)
+        gl.useProgram(program);
+        
+        // Turn on the position attribute
+        gl.enableVertexAttribArray(positionLocation);
+        
+        // Bind the position buffer.
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        
+        // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+        var size = 3;          // 3 components per iteration
+        var type = gl.FLOAT;   // the data is 32bit floats
+        var normalize = false; // don't normalize the data
+        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;        // start at the beginning of the buffer
+        gl.vertexAttribPointer(
+            positionLocation, size, type, normalize, stride, offset);
+        
+        // Turn on the color attribute
+        gl.enableVertexAttribArray(colorLocation);
+        
+        // Bind the color buffer.
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        
+        // Tell the attribute how to get data out of colorBuffer (ARRAY_BUFFER)
+        var size = 3;                 // 3 components per iteration
+        var type = gl.UNSIGNED_BYTE;  // the data is 8bit unsigned values
+        var normalize = true;         // normalize the data (convert from 0-255 to 0-1)
+        var stride = 0;               // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;               // start at the beginning of the buffer
+        gl.vertexAttribPointer(
+            colorLocation, size, type, normalize, stride, offset);
+        
+        var numFs = 5;
+        var radius = 200;
+        
+        // Compute the projection matrix
+        var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+        var zNear = 1;
+        var zFar = 2000;
+        var projectionMatrix = webglm4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
+        
+        // Compute a matrix for the camera
+        var cameraMatrix = webglm4.yRotation(cameraAngleRadiansY);
+        cameraMatrix = webglm4.xRotate(cameraMatrix, cameraAngleRadiansX);
+        cameraMatrix = webglm4.translate(cameraMatrix, 0, 0, radius * 1.5);
+        
+        // Make a view matrix from the camera matrix
+        var viewMatrix = webglm4.inverse(cameraMatrix);
+        
+        // Compute a view projection matrix
+        var viewProjectionMatrix = webglm4.multiply(projectionMatrix, viewMatrix);
+        
+        // Set the matrix.
+        gl.uniformMatrix4fv(matrixLocation, false, viewProjectionMatrix);
+        
+        // Draw the geometry.
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0;
+        var count = 6 * 6;
+        gl.drawArrays(primitiveType, offset, count);
+    }
+
+    return {
+        drawInit: drawInit,
+        drawScene: drawScene,
+        AddCameraAngle: AddCameraAngle,
+        AddCameraView: AddCameraView,
+    };
+  }));
+  
